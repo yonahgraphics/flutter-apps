53359dc8f218f759e8726f1f88b8d13329720096
yonahgraphics <yonahbyarugaba@gmail.com>
2021-03-24 22:37:04 +0200

    first commit

---
+++ bmi_calculator/lib/components/bottom_button.dart
@@ -0,0 +1,28 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({@required this.buttonTitle, @required this.onTap});
+
+  final String buttonTitle;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: kPink80,
+        margin: EdgeInsets.only(top: 10.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kLargeButtonStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ bmi_calculator/lib/constants.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xFF111328);
+const kPink20 = Color(0x29EB1555);
+const kPink80 = Color(0xFFEB1555);
+const kGrey90 = Color(0xFF8D8E98);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: kGrey90,
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kLargeButtonStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
